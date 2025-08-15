4d1257c3d7d27760e365cdc04b13c3e8bb5100bc
LucasGS520 <lucasgsilva520@gmail.com>
2025-08-15 11:59:01 -0300

    docs: Add docstring de explicação ao pacote principal do market_scraper

---
+++ market_scraper/__init__.py
@@ -0,0 +1,5 @@
+""" Pacote responsável pelas funcionalidades de scraping de produtos
+
+Concentra rotas, serviços e utilitários utilizados para coletar dados de
+sites externos e disponibilizá-los por meio da API.
+"""
