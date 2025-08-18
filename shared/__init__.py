07c6e25eec940f11f3054fa25602a84e10d61798
LucasGS520 <lucasgsilva520@gmail.com>
2025-08-18 17:24:02 -0300

    docs: Add docstrings ao pacote shared, com breve explicações sobre itens comuns do projeto

---
+++ shared/__init__.py
@@ -0,0 +1,1 @@
+""" Pacote que reúne utilitários e componentes comuns utilizados em diferentes partes do projeto """
---
+++ shared/tests/__init__.py
@@ -0,0 +1,1 @@
+""" Conjunto de testes que valida os utilitários e componentes compartilhados do pacote `shared` """
